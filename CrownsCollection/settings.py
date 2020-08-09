a98dcb369028331f5e851dc1b3d00b601d77bfb6
deyundanprodemy <obikpidaniel@gmail.com>
2020-08-09 14:40:01 +0100

    commit

--- CrownsCollection/settings.py
+++ CrownsCollection/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's=&8vd8a^+j!jhb51an2$#2y%m=k&8gqu7gnc62i06v07pu6mm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
